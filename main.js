b2f2ae4251d9323f693999b019ec7b8b8639d191
nobuhiroomasa <ca01233006@st.kawahara.ac.jp>
2024-11-29 11:54:13 +0900

    修正

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 
-import { setupCounter } from './counter.js'
+import { setupCounter } from './counter.js';
 
 
 
-document.querySelector('#header').innerHTML = `
+// document.querySelector('#header').innerHTML = `
 
-      <ul class="header">
-          <li class="asteroid"><a href="demo-repository/Asteroid/asteroid.htm">Asteroid</a></li>
-          <li class="Blocks"><a href="Blocks/blocks.htm">Blocks</a></li>
-          <li class="MineSweeper"><a href="MineSweeper/minesweeper.html">MineSweeper</a></li>
-          <li class="Cave"><a href="Cave/cave.html">Cave</a></li>
-          <li class="MissileCommand"><a href="MissileCommand/missilecommand.htm">MissileCommand</a></li>
-          <li class="Qix"><a href="Qix/Qix.htm">Qix</a></li>
-          <li class="Invader"><a href="Invader/SpaceInvader.htm">Invader</a></li>
-          <li class="Memorizer"><a href="Memorizer/memorizer.html">Memorizer</a></li>
-          <li class="SnakeBite"><a href="SnakeBite/SnakeBite.html">SnakeBite</a></li>
-          <li class="Tetris/Tetris.htm">Tetris</a></li>
-          <li><a href="index.html">Top</a></li>
+//       <ul class="header">
+//           <li class="asteroid"><a href="demo-repository/Asteroid/asteroid.htm">Asteroid</a></li>
+//           <li class="Blocks"><a href="Blocks/blocks.htm">Blocks</a></li>
+//           <li class="MineSweeper"><a href="MineSweeper/minesweeper.html">MineSweeper</a></li>
+//           <li class="Cave"><a href="Cave/cave.html">Cave</a></li>
+//           <li class="MissileCommand"><a href="MissileCommand/missilecommand.htm">MissileCommand</a></li>
+//           <li class="Qix"><a href="Qix/Qix.htm">Qix</a></li>
+//           <li class="Invader"><a href="Invader/SpaceInvader.htm">Invader</a></li>
+//           <li class="Memorizer"><a href="Memorizer/memorizer.html">Memorizer</a></li>
+//           <li class="SnakeBite"><a href="SnakeBite/SnakeBite.html">SnakeBite</a></li>
+//           <li class="Tetris/Tetris.htm">Tetris</a></li>
+//           <li><a href="index.html">Top</a></li>
 
-      </ul>
+//       </ul>
 
       
+document.querySelector('#footer').innerHTML = `
 
-`
+      <div class="footer-content">
+        <p>© 2024 ミニゲーム集. All Rights Reserved.</p>
+        <nav class="footer-nav">
+          <a href="/privacy-policy.html">プライバシーポリシー</a>
+          <a href="/terms-of-service.html">利用規約</a>
+          <a href="/contact.html">お問い合わせ</a>
+        </nav>
+      </div>
+      `;
 
-// setupCounter(document.querySelector('#counter'))
+      function loadCSS(href) {
+        const baseURL = import.meta.env.BASE_URL; // ViteのベースURL
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = baseURL + href; // ベースURL + 相対パス
+        document.head.appendChild(link);
+      }
 
-{/* <div class="account-container">
-<div class="account-name" id="account-name"></div> <!-- アカウント名を表示する部分 -->
-<div class="dropdown-menu" id="dropdown-menu">
-    <div class="nickname-input">
-        <input type="text" id="nickname" placeholder="ニックネームを入力" />
-        <button id="save-nickname">保存</button>
-    </div>
-</div>
-</div> */}
+      loadCSS('main.css');
+
