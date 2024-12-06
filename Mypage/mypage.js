3c4fc003827c40ddcfb03d49d60a2235dce8af90
nobuhiroomasa <ca01233006@st.kawahara.ac.jp>
2024-12-06 11:38:14 +0900

    修正

--- Mypage/mypage.js
+++ Mypage/mypage.js
@@ -1,33 +1,58 @@
-import {getUserEmail, nickname, getUserScoresByEmail,getUsertotleScoresByEmail} from "../firebaseConfig.js";
+import {getUserEmail, nickname, getUserScoresByEmail,getUsertotleScoresByEmail, getScoreRank} from "../firebaseConfig.js";
 
+window.addEventListener("load",  async function () {
 
-        const email = await getUserEmail();
-        const Nickname = await nickname(email);
         
         const Myname = document.getElementById("username");
         const Myemail = document.getElementById("email");
         const gameList = document.getElementById('game-list');
+        const loading = document.getElementById("loading");
         const collections = ['Asteroid', 'Blocks', 'MineSweeper','Cave','Missile Command','Qix','Invader','Memorizer','SnakeBite','Tetris'];
 
-        Myname.innerText = Nickname;
-        Myemail.innerText = email;
+        try{
+            const email = await getUserEmail();
+            const Nickname = await nickname(email);
+            const totle = await getUsertotleScoresByEmail(email);
+            let totlescore = document.getElementById("totleScore");
+            const rank = getScoreRank();
 
-        getUserScoresByEmail(email,collections).then((data) => {
-            for(const col of collections){
-                const gameItem = document.createElement("p");
-                gameItem.id = col;
-                gameList.appendChild(gameItem);
-                let gemaname = document.getElementById(col);
-                if( data[col] != null){
-                    gemaname.innerText = `${col} - スコア: ${data[col]}`;
+            Myname.innerText = Nickname;
+            Myemail.innerText = email;
+
+            getUserScoresByEmail(email,collections).then((data) => {
+                for(const col of collections){
+                    const gameItem = document.createElement("p");
+                    gameItem.id = col;
+                    gameList.appendChild(gameItem);
+                    let gemaname = document.getElementById(col);
+                    if( data[col] != null){
+                        gemaname.innerText = `${col} - スコア: ${data[col]}`;
+                    }
+                    
                 }
-                
-            }
-        })
+            })
+            totlescore.innerText = `トータルスコア: ${totle}`;
+            console.log(rank.userranks);
+            console.log(rank.useralls);
 
-        const totle = await getUsertotleScoresByEmail(email);
-        let totlescore = document.getElementById("totleScore");
-        totlescore.innerText = `トータルスコア: ${totle}`;
+        }catch(e){
+            console.error("エラーが発生しました：", e);
+
+        }finally{
+            loading.style.display = "none";
+        }
+
+
+
+        
+
+        
+            // ローディング画面を非表示にしてメインコンテンツを表示
+
+        });
+        
+
+
 
         
 
