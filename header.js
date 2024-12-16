1db25b09d81543d025165afdb08a64c86b0f8cf7
nobuhiroomasa <ca01233006@st.kawahara.ac.jp>
2024-12-16 15:38:40 +0900

    追加

--- header.js
+++ header.js
@@ -2,9 +2,13 @@
 
 // ヘッダーHTMLをテンプレートリテラルとして定義
 const headerHTML = `
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <header>
   <ul class="header">
-        <li><a href="Asteroid/asteroid.html">Asteroid</a></li>
+        <li><a href="/Asteroid/asteroid.html">Asteroid</a></li>
         <li><a href="/Bloks Game/bloks game.html">Blocks</a></li>
         <li><a href="/Cave/cave.html">Cave</a></li>
         <li><a href="/SpaceInvader/spaceinvader.html">Invader</a></li>
